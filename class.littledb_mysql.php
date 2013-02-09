6d6cc154718acc08ff494a723adabc28b57d2b9b
Cody Alberto Roodaka <roodakazo@hotmail.com>
2013-02-08 21:59:38 -0300

    Agregados cambios mayores, implemtenación de mysqli

--- class.littledb_mysql.php
+++ class.littledb_mysql.php
@@ -1,6 +1,6 @@
 <?php
 /* *****************************************************************************
- ***                           Clase LittleDB 2.1                            ***
+ ***                           Clase LittleDB 1.8                            ***
  **************************************************************************** */
 class LittleDB
  {
@@ -54,6 +54,22 @@ class LittleDB
 
 
   /**
+   * Obtenemos una instancia de la clase, la primera vez pasamos los parametros para conectar.
+   * @return object $instance Instancia de la base de datos.
+   * @author Ignacio Daniel Rostagno <ignaciorostagno@vijona.com.ar>
+   */
+  public static function getInstance($host = NULL, $user = NULL, $pass = NULL, $db = NULL)
+   {
+    if(!isset(self::$instance))
+     {
+      self::$instance = new LittleDB($host, $user, $pass, $db);
+     }
+    return self::$instance;
+   } // public static function getInstance();
+
+
+
+  /**
    * No se permite clonar el objeto.
    * @author Ignacio Daniel Rostagno <ignaciorostagno@vijona.com.ar>
    */
@@ -70,34 +86,6 @@ class LittleDB
 
 
   /**
-   * Destruir & Desconectar la clase
-   * @author Cody Roodaka <roodakazo@hotmail.com>
-   */
-  public function __destruct()
-   {
-    if($this->conn != null) { return mysqli_close($this->conn); }
-    else { return false; }
-   } // public function __destruct()
-
-
-
-  /**
-   * Obtenemos una instancia de la clase, la primera vez pasamos los parametros para conectar.
-   * @return object $instance Instancia de la base de datos.
-   * @author Ignacio Daniel Rostagno <ignaciorostagno@vijona.com.ar>
-   */
-  public static function get_instance($host = NULL, $user = NULL, $pass = NULL, $db = NULL)
-   {
-    if(!isset(self::$instance))
-     {
-      self::$instance = new LittleDB($host, $user, $pass, $db);
-     }
-    return self::$instance;
-   } // public static function getInstance();
-
-
-
-  /**
    * Conectar al servidor y seleccionar la DB
    * @author Cody Roodaka <roodakazo@hotmail.com>
    */
@@ -105,7 +93,8 @@ class LittleDB
    {
     if($this->is_connected() == false)
      {
-      $this->conn = mysqli_connect($this->host, $this->user, $this->pass, $this->db) or exit('No se ha podido conectar al servidor.');
+      $this->conn = mysql_connect($this->host, $this->user, $this->pass) or die('Error: No se ha podido conectar al servidor.');
+      mysql_select_db($this->db, $this->conn) or die('Error: Base de datos inexistente.');
      }
    } // public function connect();
 
@@ -117,8 +106,8 @@ class LittleDB
    */
   private function is_connected()
    {
-    if(is_object($this->conn)) { return true; }
-    else { return false; }
+    if(!is_resource($this->conn) || $this->conn == null || !$this->conn) { return false; }
+    else { return true; }
    }
 
 
@@ -143,12 +132,16 @@ class LittleDB
       if($ret == true)
        {
         $res = $this->_query($query);
-        if($res->num_rows !== 0)
+        if($res == false || empty($res)) { $return = false; }
+        else
          {
-          $return = $res->fetch_assoc();
-          $res->free();
+          if(is_resource($res))
+           {
+            $return = mysql_fetch_array($res, MYSQL_ASSOC);
+            $this->free($res);
+           }
+          else { $return = true; }
          }
-        else { $return = false; }
        }
       else
        {
@@ -159,6 +152,19 @@ class LittleDB
      }
     else { return false; }
    } // public function query();
+
+
+
+  /**
+   * Liberar memoria asociada a la conección
+   * @author Cody Roodaka <roodakazo@hotmail.com>
+   * @param resource query Cosulta SQL
+   * @return boolean
+   */
+  private function free($query)
+   {
+    return mysql_free_result($query);
+   } // private function free();
 
 
 
@@ -179,7 +185,7 @@ class LittleDB
       $query = $this->_query('INSERT INTO '.$this->db.'.'.$this->prefix.$table.' ( '.$fields.' ) VALUES ( '.$values.' )');
 
       // Seteamos el resultado,
-      if($return == true) { return $this->conn->insert_id; }
+      if($return == true) { return mysql_insert_id($this->conn); }
       else
        {
         if(!$query || $query == false) { return false; }
@@ -210,7 +216,7 @@ class LittleDB
        }
       $conditions = implode(' && ', $where);
       $query = $this->_query('DELETE FROM '.$this->db.'.'.$this->prefix.$table.' WHERE '.$conditions);
-      if($return == true) { return $this->conn->affected_rows; }
+      if($return == true) { return mysql_affected_rows($this->conn); }
       else
        {
         if(!$query || $query == false) { return false; }
@@ -243,7 +249,7 @@ class LittleDB
       foreach($cond as $field => $value) { $wher[] = $field.' = '.$this->parse_input($value); }
       $where = implode(' && ', $wher);
       $query = $this->_query('UPDATE '.$this->db.'.'.$this->prefix.$table.' SET '.$fields.' WHERE '.$where);
-      if($return == true) { return $this->conn->affected_rows; }
+      if($return == true) { return mysql_affected_rows($this->conn); }
       else
        {
         if(!$query || $query == false) { return false; }
@@ -267,15 +273,9 @@ class LittleDB
      {
       call_user_func_array($this->logger, array($query));
      }
-    return mysqli_query($this->conn, $query);
+    return mysql_query($query, $this->conn);
    } // private function _query()
 
-
-
-  public function error()
-   {
-    return mysqli_error($this->conn);
-   }
 
 
   /**
@@ -292,13 +292,13 @@ class LittleDB
     //Validamos que los parametros sea un arreglo.
     if(!is_array($params))
      {
-      throw new Exception('El parametro debe ser un arreglo válido');
+      throw new Exception("El parametro debe ser un arreglo válido");
       return $q;
      }
     //Validamos que tengamos igual numero de parametros que de los necesarios.
     if(count($params) != preg_match_all("/\?/", $q, $aux))
      {
-      throw new Exception('No coinciden la cantidad de parametros necesarios con los provistos en '.$q);
+      throw new Exception("No coinciden la cantidad de parametros necesarios con los provistos");
       return $q;
      }
     //Reemplazamos las etiquetas.
@@ -341,9 +341,21 @@ class LittleDB
      }
     else
      { //Suponemos una cadena
-      return '\''.mysqli_real_escape_string($this->conn, $object).'\'';
+      return "\"".mysql_real_escape_string($object)."\"";
      }
    } // protected function parse_input();
+
+
+
+  /**
+   * Destruir & Desconectar la clase
+   * @author Cody Roodaka <roodakazo@hotmail.com>
+   */
+  public function __destruct()
+   {
+    if($this->conn != null) { return mysql_close($this->conn); }
+    else { return false; }
+   }
  } // class LittleDB();
 
 // =============================================================================
@@ -365,9 +377,9 @@ class Query
    */
   public function __construct($query, $conn)
    {
-    $cons = mysqli_query($conn, $query);
-    if(is_object($cons)) { $this->query = $cons; return true; }
-    else { return false; }
+    $cons = mysql_query($query, $conn);
+    if($cons == false || !$cons || empty($cons)) { return false; }
+    else { $this->query = $cons; }
    } // function __construct();
 
 
@@ -376,9 +388,14 @@ class Query
    * Devolvemos el array con los datos de la consulta
    * @author Cody Roodaka <roodakazo@hotmail.com>
    */
-  public function fetchrow()
+  public function fetchrow($object = false)
    {
-    return $this->query->fetch_assoc();
+    $query = mysql_fetch_array($this->query, MYSQL_ASSOC);
+    if($object == false)
+     {
+      return $query;
+     }
+    else { return (object) $query; }
    } // public function fetchrow();
 
 
@@ -389,7 +406,7 @@ class Query
    */
   public function numrows()
   {
-   return $this->query->num_rows;
+   return mysql_num_rows($this->query);
   } // public function numrows();
 
 
@@ -403,23 +420,8 @@ class Query
    */
   public function get($field, $default)
    {
-    $result = $this->query->fetch_array(MYSQL_ASSOC);
+    $result = mysql_fetch_array($this->query, MYSQL_ASSOC);
     if(isset($result[$field])) { return $result[$field]; }
     else { return $default; }
    } // public function get();
-
-
-
-  /**
-   * Cuando destruimos el objeto limpiamos la consulta.
-   * @author Ignacio Daniel Rostagno <ignaciorostagno@vijona.com.ar>
-   */
-  public function __destruct()
-   {
-    if(is_resource($this->query))
-     {
-      $this->query->free();
-     }
-   } // public function __destruct();
-
  } // class Query
